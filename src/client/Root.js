daad4c26af0e867775f630af53ad00e771d5b70c
aikiryu <aikiryu1228@naver.com>
2020-03-16 16:19:31 +1300

    0316

--- src/client/Root.js
+++ src/client/Root.js
@@ -3,6 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "shared/App";
 
 /*
+
  그 다음, Root 컴포넌트를 만든다. 
  이 컴포넌트는 우리의 웹어플리케이션에 Browser Router를 적용한다.
  나중에 리덕스를 적용하게 될 때, 여기서 Provider를 통하여 프로젝트에 
